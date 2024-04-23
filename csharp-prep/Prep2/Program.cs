5bedb660cad64f0cdd78056d5dc2313328397a42
Timilehin Daramola <timi.homezone111@gmail.com>
2024-04-23 13:19:22 -0400

    cse210

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+        string grade_symbol = "";
+        string grade_sign = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (percent % 10 >=7)
+        {
+            grade_sign  = letter;
+        }
+        else if (percent % 10 >=3 && percent % 10 <=6)
+        {
+            grade_sign  = letter;
+        }
+        else 
+        {
+            grade_symbol = "-";
+            grade_sign  = letter + grade_symbol;
+        }
+
+        Console.WriteLine($"Your grade is: {grade_sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
