6e1dcfe604194bacb83f623031ab41bd53754814
Timilehin Daramola <timi.homezone111@gmail.com>
2024-05-11 19:03:33 -0400

    cse210

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,53 @@
+//Student: Timilehin Daramola | Creativity: I added an extra feature to make the program continuously prompt the users for more tries without breaking out of the code until 'quit' is entered.
+
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 class Program
 {
+    static Random random = new Random();
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        string userInput = "";
+        List<string> scriptures = new List<string> {"Proverbs 3:5-6", "Trust", "in the", "LORD with", "all your", "heart and lean", "not on", "your own", "understanding;", "in all your", "ways submit", "to him,", "and he will", "make your", "paths", "straight."};
+
+        foreach(string scripture in scriptures)
+        {
+            Console.Write(scripture + " ");          
+        }
+        
+        Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
+        userInput = Console.ReadLine();
+
+        while (userInput.ToLower() != "quit")
+        {
+            if(userInput == "")
+            {
+                List<string> newWord = new List<string>();
+                foreach(string scripture in scriptures)
+                {
+                    if (random.Next(5) == 0)
+                        newWord.Add(new string('_', scripture.Length));
+                    else
+                        newWord.Add(scripture);
+                }
+                Console.WriteLine(string.Join(" ", newWord));
+                Console.WriteLine("\nPress enter to continue or type 'quit' to finish: ");
+                userInput = Console.ReadLine();
+            }
+            else
+            {
+                clearConsole();
+            }
+        }
+        
+    }
+
+    static void clearConsole()
+    {
+        Console.Clear();
     }
 }
